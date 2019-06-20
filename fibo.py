b7fcb05362628bf9a1ed585df30e544697e0a86e
Satoshi <satoshi_m313@gmail.com>
2019-06-20 12:26:20 +0900

    2019/06/20 Updated This is initial commit.

---
+++ fibo.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+def fib(n):
+    """フィボナッチ数を表示する"""
+    a, b = 0, 1
+    while b < n:
+        print ("b = ", b)
+        a, b = b, a+b
+    print()
+
+def fib2(n):
+    """フィボナッチ数のリストを返す"""
+    ans = []
+    a, b = 0, 1
+    while b < n:
+        ans.append(b)
+        a, b = b, a+b
+    return ans
+
